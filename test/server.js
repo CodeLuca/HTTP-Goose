2394bbe497fea25ffa6623966060137fc6263b56
xeroelixir <xeroelixiry@gmail.com>
2015-01-31 18:17:30 -0700

    New

--- test/server.js
+++ test/server.js
@@ -1,3 +1,2 @@
-var httpgoose = require("../lib")();
-
-httpgoose.test();
+var httpgoose = require("../lib")({"user":"public_api", "pass":"public_api", "path":"chaxo"});
+var http = require("http");
