bdd66f0f83ca632e2a81120d4b7301382f146d38
Jamen Marz <jamen.marz@gmail.com>
2015-02-13 23:10:03 -0700

    Whoa, weird things are happening...  I think I was up load one night and messed up some code

--- test/server.js
+++ test/server.js
@@ -1,8 +1,7 @@
 var goose = require("../lib")({"user":"public_api", "pass":"public_api", "name":"chaxo"});
 
 goose.createServer(function(req, res){
-  res.goose.write("Test");
-  res.write("test again");
+  res.goose.write("Hello");
   res.end();
 });
 
