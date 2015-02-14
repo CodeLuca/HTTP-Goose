04fe4745b7d5b96c8d0f8ae064c815fa0e6402b1
Jamen Marz <jamen.marz@gmail.com>
2015-02-14 01:14:58 -0700

    Works! Although there is a lot of work to be done...

--- test/server.js
+++ test/server.js
@@ -1,12 +1,14 @@
-var goose = require("../lib")({"name":"chaxo"});
+var goose = require("../lib")({"user":"public_api", "pass":"public_api", "name":"chaxo"});
 
 goose.mongo.on("error", function(err){
-  console.log(err)
+  console.log(err);
 });
 
 goose.createServer(function(req, res){
-  res.goose.write("Hello");
+  res.writeHead(200, {"Content-Type": "application/json"});
+  depth = req.url.split("/"); depth.splice(0,1)
+  res.goose.findAndWrite(depth[0], new RegExp("^" + depth[1] + "$", depth[2]||"g"));
   res.end();
 });
 
-goose.listen(80);
+goose.listen(81);
